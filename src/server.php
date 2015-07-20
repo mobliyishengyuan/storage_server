50c6ae65f37b441eac54ce7ae85c36f16e7d95d7
mobliyishengyuan <moliyishengyuan@126.com>
2015-07-20 18:34:46 +0800

    Update server.php

--- src/server.php
+++ src/server.php
@@ -2,6 +2,8 @@
 class Storage_Server {
     private $serv;
     
+    private $storage_pool = array();
+    
     public function __construct() {
         $this->serv = new swoole_server('0.0.0.0', 8001);
         $this->serv->set(array(
@@ -40,6 +42,24 @@ class Storage_Server {
         $head_arr = unpack('Sid/Sversion/Llog_id/a16provider/Lmagic_num/Lreserved', $head_str);
         $body_str = trim(substr($data, 32));
         $body_arr = msgpack_unpack($body_str);
+        
+        $key = $body_arr['k'];
+        $storage = $this->getStorage($key);
+        
+        switch ($head_arr['reserved']) {
+            case 1:
+                $rtn = $storage->set($key, $body_arr['v']);
+                break;
+            case 2:
+                $rtn = $storage->get($key);
+                break;
+            default:
+                ;
+        }
+    }
+    
+    protected function getStorage($key) {
+        
     }
     
     public function onClose($serv, $fd, $from_id) {
